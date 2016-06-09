4d01e984f23bf5803a215e2d024621858bd947be
Joakim Hedlund <contact@joakimhedlund.com>
2016-06-09 22:16:14 +0200

    Remove unused property

--- src/config/config.php
+++ src/config/config.php
@@ -1,8 +1,6 @@
 <?php
 
 return array(
-  // Not really used, but might support other JSON-friendly storage engines in the future?
-  'storage' => 'elasticsearch',
 
   'elasticsearch' => array(
     'hosts' => array(
